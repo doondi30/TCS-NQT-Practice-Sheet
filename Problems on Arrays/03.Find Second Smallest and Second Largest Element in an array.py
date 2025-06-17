b69ca239e2f95178e5b3db701b10118624e7107c
R DOONDI GANGADHAR <160998038+doondigangadhar30@users.noreply.github.com>
2025-06-18 00:59:03 +0530

    Update 03.Find Second Smallest and Second Largest Element in an array.py

--- Problems on Arrays/03.Find Second Smallest and Second Largest Element in an array.py
+++ Problems on Arrays/03.Find Second Smallest and Second Largest Element in an array.py
@@ -6,3 +6,17 @@ large = arr[-2]
 print("Second smallest is", small)
 print("Second largest is", large)
 
+#optimal
+
+def secondSmallest(arr, n):
+    if (n < 2):
+        return -1
+    small = float('inf')
+    second_small = float('inf')
+    for i in range(n):
+        if (arr[i] < small):
+            second_small = small
+            small = arr[i]
+        elif (arr[i] < second_small and arr[i] != small):
+            second_small = arr[i]
+    return second_small
