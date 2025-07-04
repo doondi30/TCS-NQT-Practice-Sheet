cfd7c70837b656c1be7cd292dbdda33c31c28ffd
R DOONDI GANGADHAR <160998038+doondi30@users.noreply.github.com>
2025-07-05 02:01:31 +0530

    Create 33.Calculate the Area of the Circle.py

---
+++ Problems on Numbers/33.Calculate the Area of the Circle.py
@@ -0,0 +1,17 @@
+# Problem Statement: Given the radius of the circle, calculate the area of the circle.
+
+# Examples:
+
+# Example 1:
+# Input: N = 5
+# Output: 78.5
+# Explanation: Using formula  πr2 for finding area of circle we get area as 78.5
+
+# Example 2:
+# Input: N = 4
+# Output: 50.2
+# Explanation: Using formula  πr2 for finding area of circle we get area as 50.2
+
+n=4
+pi=3.14
+print(round(pi*(n*n), 1))
